863d7c226d73cdd7040b38d5e4b6afb8f8b7a1f9
Igor Dominiak <igor.dominiak@netguru.pl>
2014-10-25 18:39:50 +0200

    update Client spec

--- spec/lib/toggl_client/client_spec.rb
+++ spec/lib/toggl_client/client_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Toggl::Client do
-  subject { described_class.new(api_token) }
+  subject { described_class }
   let(:api_token) { 'token' }
 
   describe '#connection' do
     it 'contains proper headers' do
+      connection = subject.connection(api_token)
       keys = %w(Accept Content-Type Authorization)
-      headers = subject.headers
+      headers = connection.headers
 
       expect(keys.all? { |k| headers.key?(k) }).to be_truthy
     end
